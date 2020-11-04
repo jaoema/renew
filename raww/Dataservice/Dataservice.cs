d1449d7820c8d05f10fc8f3c0655e7afdd69c19c
jaoema <jaoema@ruc.dk>
2020-11-04 12:58:20 +0100

    movie fix

--- raww/Dataservice/Dataservice.cs
+++ raww/Dataservice/Dataservice.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Dataservice
+namespace DataserviceLib
 {
     public class Dataservice
     {
--- raww/raww/Controllers/MoviesController.cs
+++ raww/raww/Controllers/MoviesController.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataserviceLib;
 
 namespace raww.Controllers
 {
-    public class MoviesController
+    [ApiController]
+    public class MoviesController : ControllerBase
     {
+        [HttpGet("api/movie/{tconst}")]
+        public IActionResult GetMovie(string tconst)
+        {
+            var ds = new Dataservice();
+            var movie = ds.GetMovie(tconst);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(movie);
+        }
+
+
     }
 }
