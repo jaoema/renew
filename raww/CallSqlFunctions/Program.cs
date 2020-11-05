88fe00be6a32e577578845e51f8d12a5deaa2a6f
Rebaad <33205237+ReBaad@users.noreply.github.com>
2020-11-05 11:57:57 +0100

    SQLFunctions

---
+++ raww/CallSqlFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallSqlFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
