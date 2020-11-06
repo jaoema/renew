714223a1c51a401a5fdb1260c4926f4a9865983a
Rebaad <33205237+ReBaad@users.noreply.github.com>
2020-11-06 10:38:38 +0100

    push

--- raww/CallSqlFunctions/Program.cs
+++ raww/CallSqlFunctions/Program.cs
@@ -8,21 +8,29 @@ namespace CallSqlFunctions
         static void Main(string[] args)
         {
             var connectionString = "host=localhost;db=imdb;uid=postgres;pwd =Baad666";
+            UseAdo(connectionString);
+
+        }
+
+        private static void UseAdo(string connectionString)
+        {
             var connection = new NpgsqlConnection(connectionString);
             connection.Open();
 
             //var cmd = new NpgsqlCommand("select * from find_coplayers('%ab%')", connection);
-            var cmd = new NpgsqlCommand("select * from name_search('hans1','Mads')", connection);
+            //var cmd = new NpgsqlCommand("select * from name_search('hans1','Mads')", connection);
+
+            //hans1 needs to be replaced by Username later on. So that it works for everyone.
+            var cmd = new NpgsqlCommand("select * from name_search('hans1','%ab%')", connection);
 
 
             var reader = cmd.ExecuteReader();
 
-            while(reader.Read())
+            while (reader.Read())
             {
                 Console.WriteLine($"{reader.GetString(0)}, {reader.GetString(1)}");
 
             }
-
         }
     }
 }
