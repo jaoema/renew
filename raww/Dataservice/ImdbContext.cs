05495858a439395e7cfbd9fffb81699cba996c32
Alex Østerby Jensen <Alexoej@hotmail.com>
2020-11-13 17:26:36 +0100

    functions in dataservice

---
+++ raww/Dataservice/ImdbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataserviceLib
+{
+    class ImdbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ImdbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasNoKey();
+            modelBuilder.Entity<Person>().Property(x => x.Nconst).HasColumnName("nconst");
+            modelBuilder.Entity<Person>().Property(x => x.Primaryname).HasColumnName("primaryname");
+           // modelBuilder.Entity<Person>().Property(x => x.Birthyear).HasColumnName("birthyear");
+            //modelBuilder.Entity<Person>().Property(x => x.Deathyear).HasColumnName("deathyear");
+        }
+    }
+}
--- raww/Dataservice/Person.cs
+++ raww/Dataservice/Person.cs
@@ -8,10 +8,10 @@ namespace DataserviceLib
     {
         public string Nconst { get; set; }
         public string Primaryname { get; set; }
-        public int Birthyear { get; set; }
-        public int Deathyear { get; set; }
-        public int Frequency { get; set; }
-        public Knownfortitle Knownfortitle { get; set; }
-        public Primaryprofession Primaryprofession { get; set; }
+        //public int Birthyear { get; set; }
+        //public int Deathyear { get; set; }
+        //public int Frequency { get; set; }
+        //public Knownfortitle Knownfortitle { get; set; }
+        //public Primaryprofession Primaryprofession { get; set; }
     }
 }
