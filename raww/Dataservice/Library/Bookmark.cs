2bb99de84711fbe48af06e733023fafa7e4903dd
jaoema <jaoema@ruc.dk>
2020-12-09 11:20:36 +0100

    Added data to Bookmark in class and context

--- raww/Dataservice/Library/Bookmark.cs
+++ raww/Dataservice/Library/Bookmark.cs
@@ -9,5 +9,8 @@ namespace DataserviceLib
         public string Username { get; set; }
         public string Tconst { get; set; }
         public string Nconst { get; set; }
+        public string Primarytitle { get; set; }
+        public string Primaryname { get; set; }
+        public int Startyear{ get; set; }
     }
 }
